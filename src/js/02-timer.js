c11e0b57a443287fae6c7125ac7ebf433b96a864
X777X777X <119849170+X777X777X@users.noreply.github.com>
2023-05-14 20:30:46 +0300

    fix

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -61,7 +61,6 @@ function addLeadingZero(value) {
 }
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
